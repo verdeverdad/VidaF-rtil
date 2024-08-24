9b3507a0e0771ddfb25a8f5d23ae53a6c5b5c63e
verdeverdad <sofia.16d@gmail.com>
2024-08-24 02:00:04 -0300

    inicio

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Setup and start animation maquina de escribir
 document.addEventListener('DOMContentLoaded', () => {
     var typed = new Typed('#element', {
-        strings: ['<i>Más de treinta años<br> apostando por un planeta<br> mas sustentable</i>', '<i>y hoy<br> tenemos la misma ilusión <br>que el primer día.</i>'],
+        strings: ['<i>Más de treinta años<br> trabajando por un planeta<br> mas sustentable</i>', '<i>y hoy<br> tenemos la misma ilusión <br>que el primer día.</i>'],
         typeSpeed: 50,
         loop: true,
         backDelay: 900,
