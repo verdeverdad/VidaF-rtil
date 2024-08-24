d78b303bc74d47c1290ca529e902324095530c42
verdeverdad <sofia.16d@gmail.com>
2024-08-24 01:03:50 -0300

    frase inicio

---
+++ index.js
@@ -0,0 +1,9 @@
+//Setup and start animation maquina de escribir
+document.addEventListener('DOMContentLoaded', () => {
+    var typed = new Typed('#element', {
+  strings: ['<i>mas de treinta años<br> apostando por un planeta<br> mas sustentable</i>', '<i>y hoy tenemos<br> la misma ilusión <br>que el primer día.</i>'],
+  typeSpeed: 50,
+  loop: true,
+ 
+});
+});
